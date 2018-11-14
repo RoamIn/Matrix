c986f2f71449f4a0215e5c84536d67af02b68f49
Vinci <ivill@live.com>
2018-11-14 11:15:17 +0800

    fix show all

--- backend/app/service/torrent.js
+++ backend/app/service/torrent.js
@@ -2,21 +2,26 @@ const Service = require('egg').Service
 
 class TorrentService extends Service {
     async getTorrentByTitle ({title = '', page = 1, pageSize = 10}) {
+        title = title.trim()
+
+        const hasTitle = title !== ''
+
         try {
             const db = this.ctx.model.Torrent
             const reg = new RegExp(title, 'i')
             const query = {title: {$regex: reg}}
+            const skip = hasTitle ? (pageSize * (page - 1)) : 0
 
             const result = await Promise.all([
                 db.count(query),
-                db.find(query).skip(pageSize * (page - 1)).limit(pageSize)
+                db.find(query).skip(skip).limit(pageSize)
             ])
 
             return {
                 code: 0,
                 data: {
                     list: result[1],
-                    total: result[0]
+                    total: hasTitle ? result[0] : 10
                 },
                 msg: 'success'
             }
